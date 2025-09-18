0fbe96cfd43fcf8f946b5b078ab943ff747e012f
Catasi <aideecasillas14@gmail.com>
2025-09-17 18:58:25 -0600

    dependencia concurrently

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-// console.log('Si está jalandooo');
+// import express from 'express';
+// import fetch from 'node-fetch';
 
-// const http = require('node:http')
+// const app = express();
+// app.use(express.json()); // ← AGREGAR ESTO (global)
+// const apiAlumnos = "http://localhost:4003/api/alumnos";
+// const apiProfesores = "http://localhost:4002/api/profesores";
+// const apiRH = "http://localhost:3002/api/professors";  // si es esta?
+// const apiSE = "http://localhost:3001/api/SE";
 
-// const server = http.createServer((req, res) => {
-// console.log('solicitud recivida')
-// res.end('Hellouuu, si da')
-// })
+// app.get('/', (req, res) => {
+//     res.send('Hellouuu, si da con Express');
+// });
 
-// server.listen(7020, () => {
-// console.log('Servidor escuchando el puerto 7020') 
-// })
-
-
-
-import express from 'express';
-import fetch from 'node-fetch';
-
-const app = express();
-app.use(express.json()); // ← AGREGAR ESTO (global)
-const apiAlumnos = "http://localhost:4003/api/alumnos";
-const apiProfesores = "http://localhost:4002/api/profesores";
-const apiRH = "http://localhost:3002/api/professors";  // si es esta?
-const apiSE = "http://localhost:3001/api/SE";
-
-app.get('/', (req, res) => {
-    res.send('Hellouuu, si da con Express');
-});
-
-//Inicialización del servidor
-app.listen(7020, () => {
-    console.log('Servidor escuchando el puerto 7020');
-});
+// //Inicialización del servidor
+// app.listen(7020, () => {
+//     console.log('Servidor escuchando el puerto 7020');
+// });
