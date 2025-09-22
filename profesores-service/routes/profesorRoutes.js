3b6ba8b1a7cb9339587423f56891e71e5a57b260
Sonyc343 <sonyc7500@hotmail.com>
2025-09-22 12:56:35 -0600

    ...

--- profesores-service/routes/profesorRoutes.js
+++ profesores-service/routes/profesorRoutes.js
@@ -81,14 +81,22 @@ router.post("/login", async (req, res) => {
 function authMiddleware(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Token requerido" });
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+
+    req.user = {
+      id: decoded.id || decoded.userId,
+      usuario: decoded.usuario || decoded.username,
+      role: decoded.role
+    };
+
     next();
   } catch (error) {
     return res.status(401).json({ error: "Token inválido" });
   }
 }
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
