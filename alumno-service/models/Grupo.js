5736cc284448c3103b4472bebfb328d279cf51c6
Catasi <aideecasillas14@gmail.com>
2025-09-22 19:53:51 -0600

    creacion de grupos

--- alumno-service/models/Grupo.js
+++ alumno-service/models/Grupo.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const GrupoSchema = new mongoose.Schema({
   nombre: { type: String, required: true },
   carrera: { type: String, required: true },
+  profesor_nombre: { type: String, required: true},
   alumnos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Alumno" }]
 }, { timestamps: true });
 
